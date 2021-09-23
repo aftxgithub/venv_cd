951ca207e39d8dd5bf6535a6d672803e4ea5caf2
Faithfulness Alamu <vaguemail369@gmail.com>
2021-09-23 19:49:20 +0100

    feat: implement custom cd
      - Hook into cd
      - Exectute builtin cd
      - Verify there's a virtual env
      - Source virtual env

---
+++ venv_cd.sh
@@ -0,0 +1,15 @@
+_venv_cd() {
+	builtin cd $@	# do actual directory change
+	for dir in */	# walk top-level dirs
+	do
+		dir=${dir%*/} # remove trailing '/'
+		if [ -f "$dir/bin/activate" ]
+		then
+			# source the virtual environment
+			echo "Environment found in $dir, sourcing..."
+			. $dir/bin/activate
+		fi
+	done
+}
+
+alias cd=_venv_cd
